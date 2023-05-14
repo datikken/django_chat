17b8df6034838aee6e99846ae3aeea2c19480276
datikken <shadybekov@cointelegraph.com>
2023-05-14 17:21:09 +0300

    blog added

--- therapist/therapist/urls.py
+++ therapist/therapist/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("blog.urls")),
     path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
 ]
